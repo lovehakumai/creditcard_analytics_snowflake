0709e22d304af3c2b9b08d1abe695c9ab380e3fb
lovehakumai <masaotomeza@gmail.com>
2026-05-31 13:25:02 +0000

    delete test.py and create risk_view.py

--- 03_streamlit/app.py
+++ 03_streamlit/app.py
@@ -6,9 +6,6 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
 
-from src.components.test import test_view
+from src.components.risk_view import risk_view
 
-st.title("Let me query")
-st.header("#1 input the table info you wanna get")
-
-test_view()
+risk_view()
---
+++ 03_streamlit/src/components/risk_view.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+from src.services.repository import get_target_data
+
+def risk_view():
+    db_name = "PC_DBT_DB"
+    schema_name = "DBT_MURA"
+    table_name = "STG_CREDIT_CARD"
+    
+    df = get_target_data(db_name, schema_name, table_name)
+    st.dataframe(df)
